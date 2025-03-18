2725c5bb25bb88381f7a7a0419dedbb86056d0c7
xOAviOx <avishuklacode@gmail.com>
2025-03-18 22:59:30 +0530

    Checks for flags and Threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::{env, error, net::IpAddr, str::FromStr};
+
+struct  Arguments {
+    flag: String,
+    ipaddr:IpAddr,
+    threads:u16,
+}
+
+impl Arguments {
+    fn new(args : &[String])-> Result<Arguments, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }else if args.len()>4 {
+            return  Err("too many arguments");            
+        }
+        let f = args[1].clone();
+        if let Ok(ipaddr) = IpAddr::from_str(&f){
+            return Ok(Arguments { flag: String::from(""), ipaddr, threads: 4 });
+        } else{
+            let flag = args[1].clone();
+            if flag.contains("-h") || flag.contains("-help") && args.len()==2{
+                println!("Usage: -j to select how many threads you want 
+                \r\n  -h or -help to show this help message");
+                return Err("help");
+            }else if flag.contains("-h") || flag.contains("-help"){
+                return Err("too many arguments");
+            }else if flag.contains("-j") {
+                let ipaddr= match IpAddr::from_str(&args[3]){
+                Ok(s) => s,
+                Err(_) => return Err("not a valid IPADDR; must be IPV4 or IPV6")
+                };               
+                let threads = match args[2].parse::<u16>(){
+                    Ok(s)=>s,
+                    Err(_)=> return Err("failed to parse thread number")
+                };
+            }
+        }
+    }    
+}
+
 fn main() {
-    
+    let args:Vec<String> = env::args().collect();       
+
+    let programs = args[0].clone();
+
+
 }
