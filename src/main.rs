91f4203c8d13e406af56398d2e4975a52f77df7a
xOAviOx <avishuklacode@gmail.com>
2025-03-18 23:41:34 +0530

    break out

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,10 @@ fn scan(tx:Sender<u16>, start_port: u16, addr:IpAddr, num_threads:u16 ) {
             }
             Err(_)=>{}
         }
+        if(MAX - port) <= num_threads{
+            break;
+        }
+        port += num_threads;
     }
 }
 
